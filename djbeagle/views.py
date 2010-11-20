1254831035159edd146a33df652ff20875a97557
Eric D Helms <ericdhelms@gmail.com>
2010-11-19 23:13:59 -0500

    Added base.html standard layout. Added simple initial template for homepage.

--- djbeagle/views.py
+++ djbeagle/views.py
@@ -5,7 +5,9 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
-    return render_to_response("djbeagle/base.html",
+    return render_to_response("djbeagle/home.html",
                 context_instance=RequestContext(request))
