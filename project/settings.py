a71d2b0c35f6a0992b2a28547d3690f55ecc7bdf
Eric D Helms <ericdhelms@gmail.com>
2012-09-17 22:59:06 -0400

    Updates path for openshiftlibs.

--- project/settings.py
+++ project/settings.py
@@ -110,9 +110,9 @@ default_keys = { 'SECRET_KEY': 'vm4rl5*ymb@2&d_(gc$gb-^twq9w(u69hi--%$5xrh!xk(t%
 # Replace default keys with dynamic values if we are in OpenShift
 use_keys = default_keys
 if ON_OPENSHIFT:
-    imp.find_module('openshiftlibs')
-    import openshiftlibs
-    use_keys = openshiftlibs.openshift_secure(default_keys)
+    imp.find_module('project.openshiftlibs')
+    import project.openshiftlibs
+    use_keys = project.openshiftlibs.openshift_secure(default_keys)
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = use_keys['SECRET_KEY']
