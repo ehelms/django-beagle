7ab6124eb371c8ea86bae6e51ae8cdfc8f873d25
Eric D Helms <ericdhelms@gmail.com>
2012-09-17 22:29:31 -0400

    Adds a slew of changes to accomodate deployment on OpenShift.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='django-beagle',
+    version='1.0',
+    description='',
+    author='Eric D. Helms',
+    author_email='ericdhelms@gmail.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=['Django>=1.4', 'gdata', 'BeautifulSoup', 'python-openid', 'south'],
+)
